91acc8275043e72004ddaa3e2fde7bc2206e4682
Kevin Zoltany <hello@kzoltany.me>
2019-02-03 16:07:52 -0800

    Github API working, still has issues though

--- src/Projects.js
+++ src/Projects.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
-import { Github } from 'react-social-github';
 
-var user = 'kzolt';
+/*********************************
+ * TODO: FIX THIS
+ * Data being retreived, but not rendering
+ *********************************/
 
+const Octokit = require('@octokit/rest')
+const octokit = new Octokit ()
+var count = 6;
+
+var items = [];
+
+octokit.repos.listForUser({
+  username: 'kzolt',
+  type: 'all'
+}).then(({ data, status, headers }) => {
+  // handle data
+  console.log(status);
+  console.log(headers);
+
+  for (var i = 0; i < count; i++)
+    {
+      items.push(
+        <div className="col-sm">
+          <div className="card text-white text-center bg-dark">
+            <div className="card-body">
+              <h5 className="card-title">{data[i].name}</h5>
+              <p className="card-text">{data[i].description}</p>
+              <a href={data[i].html_url} className="btn btn-light">View Source</a>
+            </div>
+          </div>
+        </div>);
+    }
+});
+
+// TODO: Follow Principles of DRY
 class Projects extends Component 
 {
   render() {
     return (
       <div>
-        <h2>Projects</h2>
-        <div class="row">
-          <div class="col">
-            <Github user={user} repo="Mocha"></Github>
-          </div>
-          <div class="col">
-            <Github user={user} repo="RoboticsThings"></Github>
-          </div>
-          <div class="col">
-            <Github user={user} repo="7-Segment-LED-Thing"></Github>
-          </div>
+        <div className="row">
+          {items[0]}
+          {items[1]}
+          {items[2]}
+        </div>
+        <div className="row">
+          {items[3]}
+          {items[4]}
+          {items[5]}
         </div>
       </div>
     );
