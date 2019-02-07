d55e745de1b9b4bf68210287ed95d549064fcd5c
Kevin Zoltany <hello@kzoltany.me>
2019-02-06 22:27:02 -0800

    Working Contact Form, Doesn't Send Emails though

--- src/contact.js
+++ src/contact.js
@@ -3,67 +3,81 @@ import React, { Component } from 'react';
 const sendgrid = require('@sendgrid/mail');
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-const msg = {
-    to: "hello@kzoltany.me",
-    from: "no-reply@kzoltany.me",
-    subject: "This is a test email",
-    text: "TEST",
-    html: "<h1>TEST<h1>"
-};
-sendgrid.send(msg);
+// const msg = {
+//     to: "hello@kzoltany.me",
+//     from: "no-reply@kzoltany.me",
+//     subject: "This is a test email",
+//     text: "TEST",
+//     html: "<h1>TEST<h1>"
+// };
+// sendgrid.send(msg);
 
 class Contact extends Component
 {
+    state = {
+        name: "",
+        email: "",
+        msg: "",
+        sent: false
+    }
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {};
+        this.HandleNameChange = this.HandleNameChange.bind(this);
+        this.HandleEmailChange = this.HandleEmailChange.bind(this);
+        this.HandleMsgChange = this.HandleMsgChange.bind(this);
+        this.SendMail = this.SendMail.bind(this);
+    }
+
+    SendMail()
+    {
+        this.setState({sent: true});
+        const msg = {
+            to: "hello@kzoltany.me",
+            from: this.state.email,
+            subject: "Email from " + this.state.name,
+            text: this.state.msg
+        };
+
+        console.log(msg);
+    }
+
+    HandleNameChange = (e) =>
+    {
+        this.setState({name: e.target.value});
+    };
+
+    HandleEmailChange = (e) =>
+    {
+        this.setState({email: e.target.value});
+    };
+
+    HandleMsgChange = (e) =>
+    {
+        this.setState({msg: e.target.value});
+    };
+
     render()
     {
         return (
             <form>
             <div className="form-group">
+            <label for="user_name">Name</label>
+              <input type="text" class="form-control" id="user_name" value={this.state.name} onChange={this.HandleNameChange} placeholder="Enter your name"/>
               <label for="user_email">Email address</label>
-              <input type="email" class="form-control" id="user_email" placeholder="Enter email"/>
+              <input type="email" class="form-control" id="user_email" value={this.state.email} onChange={this.HandleEmailChange} placeholder="Enter email"/>
             </div>
             <div className="form-group">
                 <label for="user_msg">Message</label>
-                <textarea className="form-control" id="user_msg" placeholder="Message" rows="3"></textarea>
+                <textarea className="form-control" id="user_msg" value={this.state.msg} onChange={this.HandleMsgChange} placeholder="Message" rows="3"></textarea>
             </div>
-            <button type="submit" className="btn btn-outline-light">Send Email</button>
+            <button type="button" onClick={this.SendMail} className="btn btn-outline-light">Send Email</button>
           </form>
         );
     }
 }
 
 export default Contact;
-
-/*
-TODO: USE JSX
-
-import React, { Component }     from 'react';
-import { HTMLDivElement }       from 'react-dom';
-import { HTMLFormElement }      from 'react-dom';
-import { HTMLLabelElement }     from 'react-dom';
-import { HTMLInputElement }     from 'react-dom';
-import { HTMLTextAreaElement }  from 'react-dom';
-import { HTMLButtonElement }    from 'react-dom';
-
-class Contact extends Component
-{
-    render()
-    {
-        return (
-            <HTMLFormElement>
-                <HTMLDivElement class="form-group">
-                    <HTMLLabelElement for="user_email">Email Address</HTMLLabelElement>
-                    <HTMLInputElement type="email" class="form-control" id="user_email" placeholder="Enter Email"></HTMLInputElement>
-                </HTMLDivElement>
-                <HTMLDivElement class="form-group">
-                    <HTMLLabelElement for="user_msg">Message</HTMLLabelElement>
-                    <HTMLTextAreaElement class="form-control" id="user_msg" rows="3"></HTMLTextAreaElement>
-                </HTMLDivElement>
-                <HTMLButtonElement type="submit" class="btn btn-primary">Send Email</HTMLButtonElement>
-            </HTMLFormElement>
-        );
-    }
-}
-
-export default Contact;
-*/
